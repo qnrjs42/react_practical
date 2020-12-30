eacdc7159380a2f3e0d46f1a0c2358cbc309e539
qnrjs42 <qnrjs42@naver.com>
2020-12-30 23:12:10 +0900

    2-2. 컴포넌트의 속성 값과 상태 값 분리

---
+++ ch2/ch2-2/app.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Counter from "./Counter";
+
+const App = () => {
+  const [color, setColor] = useState("red");
+
+  const onClick = () => {
+    setColor("blue");
+  };
+
+  return (
+    <div>
+      <Counter />
+      <Counter />
+      <button style={{ backgroundColor: color }} onClick={onClick}>
+        좋아요
+      </button>
+    </div>
+  );
+};
+
+export default App;
