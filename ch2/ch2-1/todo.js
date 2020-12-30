72e4ea07a9d37c9bf575f4456002d78ea0e18d62
qnrjs42 <qnrjs42@naver.com>
2020-12-30 22:44:59 +0900

    2-1. 리액트를 사용한 코드의 특징

--- ch2/ch2-1/todo.js
+++ ch2/ch2-1/todo.js
@@ -3,20 +3,20 @@ import { useState } from "react";
 const App = () => {
   const [todoList, setTodoList] = useState([]);
   const [currentId, setCurrentId] = useState(1);
-  const [desc, setDesc] = useState('');
+  const [desc, setDesc] = useState("");
   const [showOdd, setShowOdd] = useState(false);
-  
+
   const onAdd = () => {
     const todo = { id: currentId, desc };
     setCurrentId(currentId);
     setTodoList([...todoList, todo]);
-  }
+  };
 
   const onDelete = (e) => {
     const id = Number(e.target.dataset.id);
-    const newTodoList = todoList.filter(todo => todo.id !== id);
+    const newTodoList = todoList.filter((todo) => todo.id !== id);
     setTodoList(newTodoList);
-  }
+  };
 
   const onSaveToServer = () => {};
 
@@ -25,22 +25,26 @@ const App = () => {
       <h3>할 일 목록</h3>
       <ul>
         {todoList
-        .filter((_, index) => (showOdd ? index % 2 === 0 : true ))
-        .map((todo) => (
-          <li key={todo.id}>
-            <span>{todo.desc}</span>
-            <button data-id={todo.id} onClick={onDelete}>
-              삭제
-            </button>
-          </li>
-        ))}
+          .filter((_, index) => (showOdd ? index % 2 === 0 : true))
+          .map((todo) => (
+            <li key={todo.id}>
+              <span>{todo.desc}</span>
+              <button data-id={todo.id} onClick={onDelete}>
+                삭제
+              </button>
+            </li>
+          ))}
       </ul>
-      <input type="text" value={desc} onChange={(e) => setDesc(e.target.value)} />
+      <input
+        type="text"
+        value={desc}
+        onChange={(e) => setDesc(e.target.value)}
+      />
       <button onClick={onAdd}>추가</button>
       <button onClick={() => setShowOdd(!showOdd)}>홀수 On/Off</button>
       <button onClick={onSaveToServer}>서버에 저장</button>
     </>
   );
-}
+};
 
 export default App;
