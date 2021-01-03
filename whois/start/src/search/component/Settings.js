d9235424f0f8d8b1a51c10fb825ec11e2dfd4d6c
qnrjs42 <qnrjs42@naver.com>
2021-01-03 15:40:18 +0900

    7-2. 프로젝트 설정하기

---
+++ whois/start/src/search/component/Settings.js
@@ -0,0 +1,28 @@
+import { Button, Dropdown, Menu } from "antd";
+import { SettingFilled } from '@ant-design/icons';
+import React from "react";
+
+/**
+ * 
+ * @param {object} param
+ * @param {() => void} param.logout
+ */
+
+const Settings = ({ logout }) => {
+  return (
+    <>
+      <Dropdown 
+        overlay={
+          <Menu>
+            <Menu.Item onClick={logout}>로그아웃</Menu.Item>
+          </Menu>
+        } trigger={['click']}
+        placement="bottomRight"
+      >
+        <Button shape="circle" icon={<SettingFilled />} />
+      </Dropdown>
+    </>
+  );
+};
+
+export default Settings;
